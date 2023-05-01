60e7e5587a2815f19813fbce6a6d239f1c4cfc9b
Alex <elvisavfc65@gmail.com>
2023-05-01 19:25:47 -0400

    updated sweeper

--- matlab/1_code/sweeper.m
+++ matlab/1_code/sweeper.m
@@ -10,11 +10,11 @@ aux_files = [ ...
     "zeta_generator.m", "collectdnPl.m", "legendre_dx.m", ...
     "legendre_ddx.m", "collectPl.m", "my_legendre.m"];
 
-% A folder which MUST have all the dependencies needed
-safe_folder = "D:\GITRepos\km-dropplet-onto-bath\matlab\1_code\D50Quant100\rho1000sigma7220nu98muair0\RhoS1000SigmaS7220\R0350mm\ImpDefCornerAng180U38";
-
 % Initial folder to go back to
 root = pwd;
+
+% A folder which MUST have all the dependencies needed
+safe_folder = fullfile(root, "D50Quant100\rho1000sigma7220nu98muair0\RhoS1000SigmaS7220\R0350mm\ImpDefCornerAng180U38");
 
 % To force and repeat sweeps (.mat)
 force_sweep = false;
