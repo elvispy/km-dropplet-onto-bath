e00995fcc1d54b920ecb360a1063902002444998
Alex <elvisavfc65@gmail.com>
2023-05-01 21:46:10 -0400

    added first preprocessing for sweeping procedure

--- matlab/1_code/sweeper.m
+++ matlab/1_code/sweeper.m
@@ -5,8 +5,36 @@
 
 % STEP 1: Define which simulations are to be run. 
 
-% Idea: try to use a struct to add product of discrete sets and also
-% manually addind simulation cases
+D = 50;
+Quant = 100;
+rho = 1; % must multiply by x1000
+sigma = 72.20; % must multiply by x100
+muair = 0;
+RhoS = 1; % must multiply by x1000
+SigmaS = 72.20; % must multiply by x100
+R = linspace(0.02, 0.05, 5)'; % must multiply by x10
+Ang = 180;
+U = linspace(28, 50, 5)';
+
+[Didx, Quantidx, rhoidx, sigmaidx, muairidx, ...
+    RhoSidx, SigmaSidx, Ridx, Angidx, Uidx] = ...
+    ndgrid(1:length(D), 1:length(Quant), 1:length(rho), 1:length(sigma), ...
+    1:length(muair), 1:length(RhoS), 1:length(SigmaS), ...
+    1:length(R), 1:length(Ang), 1:length(U));
+
+cartesian_product = [D(Didx, :), Quant(Quantidx, :), rho(rhoidx, :), ...
+    sigma(sigmaidx, :), muair(muairidx, :), RhoS(RhoSidx, :), SigmaS(SigmaSidx, :), ...
+    R(Ridx, :), Ang(Angidx, :), U(Uidx, :)];
+% Turn simulations into table
+if isempty(cartesian_product) == true; cartesian_product = double.empty(0, 10); end
+simulations_cgs = array2table(cartesian_product, ...
+    "VariableNames", ["D", "Quant", "rho", "sigma", "muair", "RhoS", ...
+    "SigmaS", "R", "Ang", "U"]);
+% Now you can manually add any simulations that you would like to run, such
+% as:
+simulations_cgs = [simulations_cgs; ...
+    {50, 100, 1, 72.20, 0, 1, 72.20, 0.001, 180, 1}];
+
 
 % STEP 2: Recursively create folders and scripts needed
 % Exmple of folder structure:
@@ -22,6 +50,7 @@
 %               -> ImpDefCornerAng(180)U(28)
 %                   - VertPolarExactSH.m
 %                   - And many others ...
+
 
 % List of files needed to run the simulation and that do not need to change
 % from simul to simul.
