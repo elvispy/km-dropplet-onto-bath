56afefa2324fc42575850ac343a9bb8cfb636648
Alex <elvisavfc65@gmail.com>
2024-04-02 21:50:44 -0400

    added first experimental panel of figures

--- matlab/1_code/Figures/impact_panel.m
+++ matlab/1_code/Figures/impact_panel.m
@@ -9,7 +9,7 @@ global errored
 errored = ~isfile('z.mat');
 try
 
-    load('ProblemConditions.mat');
+    load('ProblemConditions.mat'); NN = N;
 catch
     load('U0.mat');
     load('Fr.mat');
@@ -36,6 +36,7 @@ load_vars('etaOri.mat')
 load_vars('tvec.mat')
 
 load_vars('oscillation_amplitudes.mat');
+load_vars('pressure_amplitudes.mat');
 Rv = zeros(1, size(oscillation_amplitudes, 2));
 for ii = 1:size(oscillation_amplitudes, 2)
     Rv(ii) = zs_from_spherical(pi, oscillation_amplitudes(:, ii));
@@ -135,17 +136,20 @@ thetaZero = 0; %%
 zb = Gamma/(Fr*wzero^2)*cos(wzero*tvec+thetaZero); %Elevation of the pool
 zbplot=zb; %(1:end-1);
 
-subplots = 7;
-figure('Position', [100, 100, 1024, 400]);
-subplotWidths = 0.8;
-subplotHeight = 0.8;
-for ii = floor(linspace(1, size(etaMatPer,2), subplots))
-
+subplots = 5;
+subplotWidths = 0.9;
+subplotHeight = 0.85;
+idxs = floor(linspace(1, size(etaMatPer,2)*0.715, subplots));
+base = 150;
+figure('Position', [5, 5, base*subplots/subplotWidth, 3*base/subplotHeight+200]);
+for jj = 1:subplots
+    ii = idxs(jj);
 
     thetaplot = linspace(0, pi, 100);%-%-0:thetaVec(end)/400:thetaVec(end);
     %-%-xsTop = xsoftheta(thetaplot,A2New,A3New);
     %-%-zsTop = zsoftheta(thetaplot,A2New,A3New);
-    position = [(i-1)*subplotWidths/subplots + (1-subplotWidths)/2, (1-subplotHeight)/2, subplotWidths/subplots, subplotHeight];
+    position = [(jj-1)*subplotWidths/subplots + (1-subplotWidths)/2, ...
+        2/3 * subplotHeight + (1-subplotHeight)/2+0.03 , subplotWidths/subplots, subplotHeight/3];
     subplot('Position', position);
     
     zsTop = zs_from_spherical(thetaplot, oscillation_amplitudes(:, ii));
@@ -157,24 +161,24 @@ for ii = floor(linspace(1, size(etaMatPer,2), subplots))
     plot([fliplr(-1*r),r(2:end)],(zbplot(ii)+[0;flipud(etaMatPer(:,ii));etaMatPer(2:end,ii);0]),...
         'color',[.4 .4 .4],'LineWidth',2)
     hold on
-    axis equal
+    
     grid on
-    set(gca,'xlim',[-5 5],'ylim',[-1.5 3],'Xtick',-5:5,'FontName','Times','FontSize',18);
-    xlabel('   $x/R_o$   ','interpreter','Latex','FontName','Times','FontSize',18)
-    ylabel('   $\frac{z}{R_o}\ \ \ $   ','interpreter','Latex','FontName','Times',...
-        'FontSize',24,'rotation',0)
-%     t = (ii-1)/360;
-%     to = floor(t);
-%     t1 = floor(10*(t-to));
-%     t2 = round(100*(t-to-t1/10));
-%     if t2 == 10
-%         t2=0;
-%         t1=t1+1;
-%     end
-%     if t1 == 10
-%         t1 = 0;
-%         to = to+1;
-%     end
+    l = 3;
+    set(gca,'xlim',[-l l],'ylim',[-(l-1) (l+1)], 'Xtick',[-2 0 2],'FontName','Times','FontSize',14);
+    %axis equal
+    %xlabel('   $x/R_o$   ','interpreter','Latex','FontName','Times','FontSize',18)
+    if jj == 1
+        %ylabel('   $\frac{z}{R_o}\ \ \ $   ','interpreter','Latex','FontName','Times',...
+        %    'FontSize',24,'rotation',0)
+        %set(gca, 'Ytick', 0);
+    else
+        
+    end
+    yticklabels("");
+        a = gca;
+    a.XRuler.TickLabelGapOffset = -4; 
+    
+    
 
     title(sprintf("$ t/t_\\sigma =\\ $ %3.2f", tvec(ii)),'FontSize',18,...
             'interpreter','latex','FontName','Times')    
@@ -182,7 +186,58 @@ for ii = floor(linspace(1, size(etaMatPer,2), subplots))
     %currFrame = getframe(gcf);
     %writeVideo(vidObj,currFrame);
     hold off
+    
+    % Pressure field distribution
+    f = zeta_generator(pressure_amplitudes(:, ii));
+    pfield = f(thetaplot) - sum(pressure_amplitudes(:, ii));
+    
+    position = [(jj-1)*subplotWidths/subplots + (1-subplotWidths)/2, ...
+        1/3 * subplotHeight + (1-subplotHeight)/2, subplotWidths/subplots, subplotHeight/3];
+    subplot('Position', position);
+    plot(thetaplot*180/pi, pfield);
+    set(gca,'xlim',[0 180], 'ylim', [-0.5, 2], 'Xtick', [45, 90, 135], ...
+        'Ytick', [0 1 2], 'FontName','Times','FontSize',14);
+    grid on
+    %axis equal
+    %xlabel('   $x/R_o$   ','interpreter','Latex','FontName','Times','FontSize',18)
+    if jj == 1
+        ylabel('$p(\theta)$ vs $\theta$','interpreter','Latex','FontName','Times',...
+            'FontSize',24,'rotation',90)
+    else
+        yticklabels("");
+        %set(gca,'ytick',[]);
+    end
+    
+    a = gca;
+    a.XRuler.TickLabelGapOffset = -4;
+
+    drawnow
+    hold off
+    
+    % Pressure amplitudes
+    position = [(jj-1)*subplotWidths/subplots + (1-subplotWidths)/2, ...
+        (1-subplotHeight)/2-0.04, subplotWidths/subplots, subplotHeight/3];
+    subplot('Position', position);
+    bar(0:NN, [-sum(pressure_amplitudes(:, ii)); pressure_amplitudes(:, ii)]);
+    set(gca, 'ylim', [-0.7, 0.7], 'Xtick', [0, floor(NN/2)], 'Ytick', [-.5 0 .5], ...
+        'FontName','Times','FontSize',14);
+    grid on
+    %axis equal
+    %xlabel('   $x/R_o$   ','interpreter','Latex','FontName','Times','FontSize',18)
+    if jj == 1
+        ylabel('$B_{\ell}$ vs $\ell $','interpreter','Latex','FontName','Times',...
+            'FontSize',24,'rotation',90)
+    else
+        yticklabels("");
+        %set(gca,'ytick',[]);
+    end
+    a = gca;
+    a.XRuler.TickLabelGapOffset = -4;
+    drawnow
+    hold off
 end
+
+
 %close(vidObj);
 
 
@@ -200,6 +255,4 @@ function load_vars(str)
     end
     
 end
-
-
 
