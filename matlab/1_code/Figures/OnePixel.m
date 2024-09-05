ace6db8f2cea3181163efee9024b404c10408dec
Elvis Aguero <elvisavfc65@gmail.com>
2024-09-05 10:51:54 -0400

    added maximum width plot for viscocity

--- matlab/1_code/Figures/OnePixel.m
+++ matlab/1_code/Figures/OnePixel.m
@@ -1,7 +1,8 @@
 close all;
 c = pwd;
+disp(mfilename('fullpath'));
 %p = uigetdir();
-addpath(fullfile(pwd, "simulation_code" ));
+addpath(fullfile(pwd, "..", "simulation_code" ));
 p = fullfile(pwd, "..",  "D50Quant100\rho1000sigma7220nu98muair0\RhoS1000SigmaS7220\R0350mm\ImpDefCornerAng180U39\N=20tol=5.00e-05");
 p = uigetdir();
 cd(p);
