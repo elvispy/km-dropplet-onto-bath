fd0f5b675d5da5ac985e3c1b0d1ae72ea5da4847
Elvis Aguero <elvisavfc65@gmail.com>
2024-09-05 09:05:21 -0400

    added failed simulation with intermediary experiment

--- matlab/1_code/sweeper_water_2024.m
+++ matlab/1_code/sweeper_water_2024.m
@@ -22,8 +22,8 @@ RhoS = 1 % must multiply by x1000 (cgs)
 SigmaS = 72.20 % must multiply by x100 (cgs)
 R = 0.035 % linspace(0.02, 0.05, 5)'; % must multiply by x10 (cgs)
 Ang = 180
-U = [45.02]' %linspace(57, 47, 6)' %inspace(59, 39, 6)';
-modes = 30
+U = [44, 44.51]' %linspace(57, 47, 6)' %inspace(59, 39, 6)';
+modes = 20
 tol = 5e-5
 
 
